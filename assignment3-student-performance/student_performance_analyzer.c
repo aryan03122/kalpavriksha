1a509585470c6a4516dfa8880ee7687c6fb69779
Aryan <312aryan@gmail.com>
2025-10-11 13:03:47 +0530

    Updated Student Performance Analyzer as per feedback

--- assignment3-student-performance/student_performance_analyzer.c
+++ assignment3-student-performance/student_performance_analyzer.c
@@ -1,129 +1,148 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<limits.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct{
+#define MAX_STUDENTS 100
+
+typedef struct {
     int rollNo;
     char name[100];
     int marks[3];
+} StudentResult;
 
-} studentResult;
+typedef enum {
+    GRADE_A,
+    GRADE_B,
+    GRADE_C,
+    GRADE_D,
+    GRADE_F
+} Grade;
 
-studentResult inputStudentDetails(){
-    studentResult student;
-    printf("\nEnter Student Roll no.: ");
-    scanf("%d" , &student.rollNo);
-    getchar();
-    printf("\nEnter Student name: ");
-    fgets(student.name , sizeof(student.name) , stdin);
-    student.name[strcspn(student.name , "\n")] = '\0';
-    printf("\nEnter Marks of students in all 3 subjects");
-    for (int i=0 ; i<3 ; i++){
-        printf("\nEnter marks in subject %d: " , i+1);
-        int validMark=-1;
-        scanf("%d" , &validMark);
-        if(validMark >= 0 && validMark<=100 ){
-            student.marks[i]= validMark;
-        }
-        else{
-            printf("\nInvalid Marks entered, Enter again");
-            i--;
+int isDuplicateRollNumber(StudentResult students[], int count, int rollNo) {
+    for (int i = 0; i < count; i++) {
+        if (students[i].rollNo == rollNo) {
+            return 1;
         }
     }
+    return 0;
+}
+
+int getValidMark(int subjectNumber) {
+    int mark;
+    while (1) {
+        printf("Enter marks in subject %d: ", subjectNumber);
+        scanf("%d", &mark);
+        if (mark >= 0 && mark <= 100) {
+            return mark;
+        }
+        printf("Invalid Marks entered, Enter again\n");
+    }
+}
+
+StudentResult inputStudentDetails(StudentResult students[], int count) {
+    StudentResult student;
+    const int numberOfSubjects = 3;
+
+    while (1) {
+        printf("\nEnter Student Roll no.: ");
+        scanf("%d", &student.rollNo);
+        getchar();
+
+        if (isDuplicateRollNumber(students, count, student.rollNo)) {
+            printf("Roll number already exists! Enter a unique roll number.\n");
+        } else {
+            break;
+        }
+    }
+
+    printf("Enter Student name: ");
+    fgets(student.name, sizeof(student.name), stdin);
+    student.name[strcspn(student.name, "\n")] = '\0';
+
+    printf("Enter Marks of student in all 3 subjects\n");
+    for (int i = 0; i < numberOfSubjects; i++) {
+        student.marks[i] = getValidMark(i + 1);
+    }
+
     return student;
 }
 
-float averageMarks(int arr[] , int n , int *totalMarks){
-float average;
-*totalMarks=0;
-for(int i=0 ; i<n ; i++){
-    *totalMarks+=arr[i];
-}
-average= (float)(*totalMarks)/n;
-return average;
+float averageMarks(int arr[], int n, int *totalMarks) {
+    *totalMarks = 0;
+    for (int i = 0; i < n; i++) {
+        *totalMarks += arr[i];
+    }
+    return (float)(*totalMarks) / n;
 }
 
-char gradeAchieved(float average){
-    if(average>=85 && average <=100){
-        return 'A';
-    }
-    else if(average>=70 && average <85){
-        return 'B';
-    }
-    else if(average>=50 && average <70){
-        return 'C';
-    }
-    else if(average>=35 && average <50){
-        return 'D';
-    }
-    else{
-        return 'F';
+Grade gradeAchieved(float average) {
+    if (average >= 85) return GRADE_A;
+    else if (average >= 70) return GRADE_B;
+    else if (average >= 50) return GRADE_C;
+    else if (average >= 35) return GRADE_D;
+    else return GRADE_F;
+}
+
+void printStars(Grade grade) {
+    switch (grade) {
+        case GRADE_A: printf("*****\n"); break;
+        case GRADE_B: printf("****\n"); break;
+        case GRADE_C: printf("***\n"); break;
+        case GRADE_D: printf("**\n"); break;
+        default: printf("\n"); break;
     }
 }
 
-void printStar(float average){
-    char x = gradeAchieved(average);
-    if (x=='A'){
-        printf("*****\n");
-    }
-    else if (x=='B'){
-        printf("****\n");
-    }
-    else if (x=='C'){
-        printf("***\n");
-    }
-    else if (x=='D'){
-        printf("**\n");
-    }
-    else{
-        printf("\n");
-    }
+void displayStudentResult(StudentResult student, int numberOfSubjects) {
+    int totalMarks;
+    float average = averageMarks(student.marks, numberOfSubjects, &totalMarks);
+    Grade grade = gradeAchieved(average);
 
+    printf("\nRoll: %d\n", student.rollNo);
+    printf("Name: %s\n", student.name);
+    printf("Total: %d\n", totalMarks);
+    printf("Average: %.2f\n", average);
+
+    char gradeChar = "ABCDF"[grade];
+    printf("Grade: %c\n", gradeChar);
+
+    if (grade != GRADE_F) {
+        printf("Performance: ");
+        printStars(grade);
+    }
 }
 
-void printRollno(studentResult student[] , int index , int N){
-    if (index==N){
-        return;
-    }
-    printf("%d " , student[index].rollNo);
-    printRollno(student , index+1 , N);
+void printRollNumbers(StudentResult students[], int index, int total) {
+    if (index == total) return;
+    printf("%d ", students[index].rollNo);
+    printRollNumbers(students, index + 1, total);
 }
 
-int main(){
-    int N;
-    printf("Hello , Enter Number of Students: ");
-    scanf("%d" , &N);
-    if(N<1 || N>100){
-        printf("\nWrong Number entered");
+int main() {
+    int numberOfStudents;
+    const int numberOfSubjects = 3;
+    StudentResult students[MAX_STUDENTS];
+
+    printf("Enter Number of Students: ");
+    scanf("%d", &numberOfStudents);
+
+    if (numberOfStudents < 1 || numberOfStudents > MAX_STUDENTS) {
+        printf("Invalid number of students.\n");
         return 0;
     }
-    studentResult students[N];
-    for(int i=0 ; i<N ; i++){
-        printf("\n\nEnter Details For student %d --", i+1);
-        students[i] = inputStudentDetails();
+
+    for (int i = 0; i < numberOfStudents; i++) {
+        printf("\n--- Enter Details for Student %d ---\n", i + 1);
+        students[i] = inputStudentDetails(students, i);
     }
 
-    for(int i=0 ; i<N ; i++){
-        int totalMarks=0;
-        float average;
-        average=  averageMarks(students[i].marks , 3 , &totalMarks);
-        char grade = gradeAchieved(average);
-        printf("Roll: %d\n", students[i].rollNo);
-        printf("Name: %s\n", students[i].name);
-        printf("Total: %d\n", totalMarks);
-        printf("Average: %.2f\n", average);
-        printf("Grade: %c\n", grade);
-
-        if (grade == 'F') {
-            continue;  // skip printing stars as told in problem statement, I have also made same case in function also.
-        }
-        printf("Performance: ");
-        printStar(average);
-        printf("\n\n");
+    for (int i = 0; i < numberOfStudents; i++) {
+        displayStudentResult(students[i], numberOfSubjects);
     }
 
-    printf("Roll Numbers are: ");
-    printRollno(students , 0 , N);
-    
+    printf("\nList of Roll Numbers: ");
+    printRollNumbers(students, 0, numberOfStudents);
+    printf("\n");
+
+    return 0;
 }
