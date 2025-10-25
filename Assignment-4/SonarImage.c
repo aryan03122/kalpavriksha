e9223d0af5bd343c4e4821d8856921bdb04effae
Aryan <312aryan@gmail.com>
2025-10-25 16:16:03 +0530

    Updated with all the changes done

--- Assignment-4/SonarImage.c
+++ Assignment-4/SonarImage.c
@@ -1,34 +1,35 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-int *getAddress(int *arr , int n , int i , int j){
+int *getAddress(int *arr, int n, int i, int j) {
     return (arr + i * n + j);
 }
 
-void printArray(int *arr, int n){
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < n ; j++){
-            printf("%d " , *getAddress(arr , n , i , j));
+void printArray(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", *getAddress(arr, n, i, j));
         }
         printf("\n");
     }
 }
 
-void getrandomMatrix(int *arr , int n){
+void generateRandomMatrix(int *arr, int n) {
     srand(time(NULL));
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < n ; j++){
-            *getAddress(arr , n , i , j) = rand() % 256;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            *getAddress(arr, n, i, j) = rand() % 256;
         }
     }
 }
 
-void transposeMatrix(int *arr , int n){
-    for(int i = 0 ; i < n ; i++){
-        for(int j = i ; j < n ; j++){
-            int *a = getAddress(arr , n , i , j);
-            int *b = getAddress(arr , n , j , i);
+void transposeMatrix(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {  
+            int *a = getAddress(arr, n, i, j);
+            int *b = getAddress(arr, n, j, i);
             int temp = *a;
             *a = *b;
             *b = temp;
@@ -36,13 +37,13 @@ void transposeMatrix(int *arr , int n){
     }
 }
 
-void reverseRows(int *arr , int n){
-    for(int i = 0 ; i < n ; i++){
+void reverseRows(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
         int j = 0;
-        int k = n-1;
-        while(j < k){
-            int *a = getAddress(arr , n , i , j);
-            int *b = getAddress(arr , n , i , k);
+        int k = n - 1;
+        while (j < k) {
+            int *a = getAddress(arr, n, i, j);
+            int *b = getAddress(arr, n, i, k);
             int temp = *a;
             *a = *b;
             *b = temp;
@@ -52,89 +53,101 @@ void reverseRows(int *arr , int n){
     }
 }
 
-void rotate90Degrees(int *arr , int n){
-    transposeMatrix((int*)arr , n);
-    reverseRows((int*)arr , n);
+void rotate90Degrees(int *arr, int n) {
+    transposeMatrix(arr, n);
+    reverseRows(arr, n);
 }
 
-void applyingSmoothingFilter(int *arr, int n){
-    int prevRow[10];   
-    int currRow[10];   
-    int nextRow[10];   
-    int outRow[10];    
+void applyingSmoothingFilter(int *arr, int n) {
     
-    for(int j = 0; j < n; j++){
-        currRow[j] = *(arr + 0*n + j);
-    }
-    
-    if(n > 1){
-        for(int j = 0; j < n; j++){
-            nextRow[j] = *(arr + 1*n + j);
-        }
-    }
-    
-    for(int i = 0; i < n; i++){     
-        for(int j = 0; j < n; j++){
-            int sum = 0;
-            int count = 0;
-            for(int ki = -1; ki <= 1; ki++){
-                int li = i + ki;
-                if(li < 0 || li >= n) continue;
+    int *prevRow = (int *)calloc(n, sizeof(int));
+    int *currRow = (int *)malloc(n * sizeof(int));
+    int *nextRow = (int *)malloc(n * sizeof(int));
+    int *outRow = (int *)malloc(n * sizeof(int));
 
-                for(int kj = -1; kj <= 1; kj++){
+    
+    for (int j = 0; j < n; j++) {
+        currRow[j] = *getAddress(arr, n, 0, j);
+    }
+    if (n > 1) {
+        for (int j = 0; j < n; j++) {
+            nextRow[j] = *getAddress(arr, n, 1, j);
+        }
+    } else {
+        memset(nextRow, 0, n * sizeof(int));
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int sum = 0, count = 0;
+            for (int ki = -1; ki <= 1; ki++) {
+                int li = i + ki;
+                if (li < 0 || li >= n) continue;
+
+                for (int kj = -1; kj <= 1; kj++) {
                     int lj = j + kj;
-                    if(lj < 0 || lj >= n) continue;
+                    if (lj < 0 || lj >= n) continue;
                     int val;
-                    if(li == i - 1){
+                    if (li == i - 1) {
                         val = prevRow[lj];
-                    } else if(li == i){
+                    } else if (li == i) {
                         val = currRow[lj];
-                    } else { 
+                    } else {
                         val = nextRow[lj];
                     }
                     sum += val;
                     count++;
                 }
-            } 
-            outRow[j] = sum / count; 
-        } 
-        for(int j = 0; j < n; j++){
-            *(arr + i*n + j) = outRow[j];
-        }
-        
-        for(int j = 0; j < n; j++){
-            prevRow[j] = currRow[j];
+            }
+            outRow[j] = sum / count;
         }
 
-        if(i + 2 < n){            
-            for(int j = 0; j < n; j++){
-                currRow[j] = nextRow[j];                    
-                nextRow[j] = *(arr + (i+2)*n + j);          
+        for (int j = 0; j < n; j++) {
+            *getAddress(arr, n, i, j) = outRow[j];
+        }
+
+        
+        memcpy(prevRow, currRow, n * sizeof(int));
+
+        if (i + 2 < n) {
+            memcpy(currRow, nextRow, n * sizeof(int));
+            for (int j = 0; j < n; j++) {
+                nextRow[j] = *getAddress(arr, n, i + 2, j);
             }
         } else {
-            for(int j = 0; j < n; j++){
-                currRow[j] = nextRow[j];
-            }
+            memcpy(currRow, nextRow, n * sizeof(int));
         }
-    } 
+    }
+
+    free(prevRow);
+    free(currRow);
+    free(nextRow);
+    free(outRow);
 }
 
-int main(){
+int main() {
     int n;
     printf("Enter size N for N x N matrix: ");
-    scanf("%d" , &n);
-    int arr[n][n];
-    getrandomMatrix((int*)arr , n);
-    printf("\nGenerated array= \n");
-    printArray((int*)arr , n);
-    rotate90Degrees((int*)arr , n);
-    printf("\nArray after rotating 90 degrees= \n");
-    printArray((int*)arr , n);
-    applyingSmoothingFilter((int*)arr , n);
-    printf("\nArray after smoothing effect= \n");
-    printArray((int*)arr , n);
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * n * sizeof(int));
+    if (!arr) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    generateRandomMatrix(arr, n);
+    printf("\nGenerated array:\n");
+    printArray(arr, n);
+
+    rotate90Degrees(arr, n);
+    printf("\nArray after rotating 90 degrees:\n");
+    printArray(arr, n);
+
+    applyingSmoothingFilter(arr, n);
+    printf("\nArray after smoothing effect:\n");
+    printArray(arr, n);
+
+    free(arr);
+    return 0;
 }
-
-
-
-
